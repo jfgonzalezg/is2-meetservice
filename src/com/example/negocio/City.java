21af14996c5874b28cdbf374ec79b8ab240cb86b
nightwolf2619 <nightwolf2619@gmail.com>
2013-05-29 03:13:53 -0500

    .

---
+++ src/com/example/negocio/City.java
@@ -0,0 +1,17 @@
+package com.example.negocio;
+
+public class City {
+	private String name;
+	
+	public City(){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
