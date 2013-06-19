99993a56b78b28b5e653c8bb48b48261ada8f610
dgallegoc <dgallegoc@unal.edu.co>
2013-06-19 04:58:07 +0000

    Sin push encuesta activity

---
+++ src/meetservice/EncuestaActivity.java
@@ -0,0 +1,15 @@
+package meetservice;
+
+import com.example.meetservice2.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EncuestaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encuesta);
+    }
+}
